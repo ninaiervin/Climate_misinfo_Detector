14483c5cd8fbd66d6a6c1d1cc4e0212d5ea2f8e4
anukzc <anukzc@gmail.com>
2025-05-13 19:51:07 -0700

    adding a scikit model

---
+++ scikit_logistic_regression.py
@@ -0,0 +1,52 @@
+from sklearn.calibration import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+import exploring_data_layout as loader
+from sentence_transformers import SentenceTransformer
+
+sentence_embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+
+train_data = loader.get_data('data/train_data.jsonl')
+dev_data = loader.get_data('data/dev_data.jsonl')
+
+x_train = []
+y_train = []
+
+for i in range(len(train_data)):
+    x_train.append(train_data[i]['claim'])
+    y_train.append(train_data[i]['claim_label'])
+
+x_dev = []
+y_dev = []
+
+for i in range(len(dev_data)):
+    x_dev.append(dev_data[i]['claim'])
+    y_dev.append(dev_data[i]['claim_label'])
+
+encoded_x_train = sentence_embedding_model.encode(x_train)
+encoded_x_dev = sentence_embedding_model.encode(x_dev)
+
+le = LabelEncoder()
+'''
+0: DISPUTED
+1: NOT_ENOUGH_INFO
+2: REFUTES
+3: SUPPORTS
+'''
+encoded_y_train = le.fit_transform(y_train)
+encoded_y_dev = le.fit_transform(y_dev)
+
+
+model = LogisticRegression(class_weight='balanced', multi_class='multinomial')
+model.fit(encoded_x_train, encoded_y_train)
+y_pred = model.predict(encoded_x_dev)
+
+accuracy = accuracy_score(encoded_y_dev, y_pred)
+precision = precision_score(encoded_y_dev, y_pred, average='macro')
+recall = recall_score(encoded_y_dev, y_pred, average='macro')
+f1 = f1_score(encoded_y_dev, y_pred, average='macro')
+
+print(f"Accuracy: {accuracy}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1-score: {f1}")
